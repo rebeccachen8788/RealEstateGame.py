8008f66ebcd0e57307ebae7873139461fa974121
rebeccachen8788 <rebeccychen@gmail.com>
2022-06-03 20:47:20 -0700

    Final version without docstrings. some error.

--- RealEstateGame.py
+++ RealEstateGame.py
@@ -240,20 +240,20 @@ class Player:
 
 
 
-game = RealEstateGame()
-
-rents = [50, 50, 50, 75, 75, 75, 100, 100, 100, 150, 150, 150, 200, 200, 200, 250, 250, 250, 300, 300, 300, 350, 350, 350]
-game.create_spaces(50, rents)
-
-game.create_player("Player 1", 1000)
-game.create_player("Player 2", 1000)
-game.create_player("Player 3", 1000)
-
-game.move_player("Player 1", 6)
-game.buy_space("Player 1")
-game.move_player("Player 2", 6)
-
-print(game.get_player_account_balance("Player 1"))
-print(game.get_player_account_balance("Player 2"))
-
-print(game.check_game_over())
+# game = RealEstateGame()
+#
+# rents = [50, 50, 50, 75, 75, 75, 100, 100, 100, 150, 150, 150, 200, 200, 200, 250, 250, 250, 300, 300, 300, 350, 350, 350]
+# game.create_spaces(50, rents)
+#
+# game.create_player("Player 1", 1000)
+# game.create_player("Player 2", 1000)
+# game.create_player("Player 3", 1000)
+#
+# game.move_player("Player 1", 6)
+# game.buy_space("Player 1")
+# game.move_player("Player 2", 6)
+#
+# print(game.get_player_account_balance("Player 1"))
+# print(game.get_player_account_balance("Player 2"))
+#
+# print(game.check_game_over())
